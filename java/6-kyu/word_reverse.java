0955fbdbaad332511cc799c4754b2381bcd08c5b
Ryan Fehr <coderry4@gmail.com>
2025-08-24 14:43:06 -0500

    Solve: 6 kyu – Reverse words(Java)

---
+++ java/6-kyu/word_reverse.java
@@ -0,0 +1,26 @@
+/*Write a function that takes in a string of one or more words, and returns the same string, but with all words that have five or more letters reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+Examples:
+
+"Hey fellow warriors"  --> "Hey wollef sroirraw" 
+"This is a test        --> "This is a test" 
+"This is another test" --> "This is rehtona test" */
+
+public class SpinWords {
+
+  public String spinWords(String sentence) {
+    String[] words = sentence.split(" ");
+    StringBuilder result = new StringBuilder();
+    
+    for (String word : words){
+      if (word.length() >= 5){
+        result.append(new StringBuilder(word).reverse());
+      }
+      else{
+        result.append(word);
+      }
+      result.append(" ");
+    }
+    return result.toString().trim();
+  }
+}
